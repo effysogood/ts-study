2acdb2422d29fa1aecc1b9c70ca4e5303de6e0f8
Saeyoung Choi <effysogood@gmail.com>
2024-08-12 09:12:32 +0900

    rename: 파일명 지정

---
+++ src/2-module/2-1-what_is_class.ts
@@ -0,0 +1,13 @@
+/**
+ * Objects (<- Classes)
+ * The things you work with in code
+ * Instances of classes (based on classes)
+ * Class based creation is an alternative to using object literals!
+ */
+
+/**
+ * Classes
+ * Blueprints for objects (theoretical definition)
+ * Define how objects look like, which properties and methods they have
+ * Classes make creation of multiple, similar objects much easier
+ */
