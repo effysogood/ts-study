08d4ddb859fa116d1b3f7eecd73de30de0c626f5
Saeyoung Choi <effysogood@gmail.com>
2024-08-16 09:29:15 +0900

    feat: intersection type

---
+++ dist/4-advanced/4-1-intersection.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Intersection Types
+ */
+{
+    const e1 = {
+        name: 'effy',
+        privileges: ['create-server'],
+        startDate: new Date(),
+    };
+}
---
+++ src/4-advanced/4-1-intersection.ts
@@ -0,0 +1,28 @@
+/**
+ * Intersection Types
+ */
+
+{
+  type Admin = {
+    name: string;
+    privileges: string[];
+  };
+
+  type Employee = {
+    name: string;
+    startDate: Date;
+  };
+
+  // interface ElevatedEmployee extends Admin, Employee {}
+  type ElevatedEmployee = Admin & Employee;
+
+  const e1: ElevatedEmployee = {
+    name: 'effy',
+    privileges: ['create-server'],
+    startDate: new Date(),
+  };
+
+  type Combinable = string | number;
+  type Numeric = number | boolean;
+  type Universal = Combinable & Numeric;
+}
