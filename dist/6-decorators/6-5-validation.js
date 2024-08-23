ab416d3463757273f4811c159ca26fd624000c0c
Saeyoung Choi <effysogood@gmail.com>
2024-08-23 14:32:48 +0900

    feat: 6 강의 수강중

--- dist/6-decorators/6-5-validation.js
+++ dist/6-decorators/6-5-validation.js
@@ -1,4 +1,9 @@
 "use strict";
+/**
+ * class-validator 패키지
+ * 다양한 검사 항목, 메타프로그밍에 활용해 부가 설정 및 로직 추가 가능
+ * Nest.js, Angular.js 활용
+ */
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -9,28 +14,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     const registeredValidators = {};
     //프로퍼티 데코레이터
     function Required(target, propName) {
-        registeredValidators[target.constructor.name] = {
+        var _a, _b;
+        registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { 
             // 검사를 추가할 프로퍼티의 이름을 키로 사용
-            [propName]: ['required'],
-        };
+            [propName]: [
+                ...((_b = (_a = registeredValidators[target.constructor.name]) === null || _a === void 0 ? void 0 : _a[propName]) !== null && _b !== void 0 ? _b : []),
+                'required',
+            ] });
     }
     function PositiveNumber(target, propName) {
-        registeredValidators[target.constructor.name] = {
-            [propName]: ['positive'],
-        };
+        var _a, _b;
+        registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: [
+                ...((_b = (_a = registeredValidators[target.constructor.name]) === null || _a === void 0 ? void 0 : _a[propName]) !== null && _b !== void 0 ? _b : []),
+                'positive',
+            ] });
     }
     function validate(obj) {
         const objValidatorConfig = registeredValidators[obj.constructor.name];
         if (!objValidatorConfig) {
             return true;
         }
+        let isValid = true;
         for (const prop in objValidatorConfig) {
             for (const validator of objValidatorConfig[prop]) {
                 switch (validator) {
                     case 'required':
-                        return !!obj[prop];
+                        isValid = isValid && !!obj[prop];
                     case 'positive':
-                        return obj[prop] > 0;
+                        isValid = isValid && obj[prop] > 0;
                 }
             }
         }
